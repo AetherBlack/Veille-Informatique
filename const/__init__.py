b0c9f9709fbe1b98a385aecff3d1cf601305bdee
Black Aether <52674895+AetherBlack@users.noreply.github.com>
2020-04-11 21:46:53 +0200

    Update __init__.py

--- const/__init__.py
+++ const/__init__.py
@@ -1,10 +1,8 @@
 #Token
 TOKEN = "YOUR_BOT_TOKEN"
 #Flux RSS
-FLUX_RSS = ["https://www.google.com/alerts/feeds/10044275366631447452/1055738863244347746",
-            "https://www.google.com/alerts/feeds/10044275366631447452/5208661666063475899",
-            "https://www.google.fr/alerts/feeds/10044275366631447452/3528165564465536219",
-            "https://www.google.fr/alerts/feeds/10044275366631447452/9042324707641309434"]
+FLUX_RSS = ["REPLACE_WITH_RSS_LINK",
+            "https://www.google.com/alerts/feeds/10044275366631447452/5208661666063475899"]#<= Exemple de FluxRSS avec Google Alertes
 #CHANNEL FLUX RSS -> NOTIF ID
 CHANNEL_RSS = INT_CHANNEL
 #WAIT UNTIL NEW VERIFICATION NEWS
