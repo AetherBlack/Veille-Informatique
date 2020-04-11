0ad9fa32eef9baf40256b0102588c33037e83a3a
Black Aether <52674895+AetherBlack@users.noreply.github.com>
2020-04-11 21:45:23 +0200

    Create __init__.py

---
+++ const/__init__.py
@@ -0,0 +1,11 @@
+#Token
+TOKEN = "YOUR_BOT_TOKEN"
+#Flux RSS
+FLUX_RSS = ["https://www.google.com/alerts/feeds/10044275366631447452/1055738863244347746",
+            "https://www.google.com/alerts/feeds/10044275366631447452/5208661666063475899",
+            "https://www.google.fr/alerts/feeds/10044275366631447452/3528165564465536219",
+            "https://www.google.fr/alerts/feeds/10044275366631447452/9042324707641309434"]
+#CHANNEL FLUX RSS -> NOTIF ID
+CHANNEL_RSS = INT_CHANNEL
+#WAIT UNTIL NEW VERIFICATION NEWS
+WAIT_NEW_NEWS = 60 * 60#Each hours
