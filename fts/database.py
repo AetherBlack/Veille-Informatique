1c935a291b8c3f2df640acca1eb98e030c1c1d3c
AetherBlack <yohan.fevre@pm.me>
2022-03-18 17:42:02 +0000

    [refactor] database clean code

--- fts/database.py
+++ fts/database.py
@@ -7,13 +7,14 @@ from const import SQLITE_CREATE_DATABASE, \
     SQLITE_SELECT_NEWS_EXISTS, SQLITE_INSERT_NEWS, \
         SQLITE_DELETE_NEWS
 
+
 class Database:
 
     """
     Instance of Database
     """
 
-    def __init__(self, path, db_name):
+    def __init__(self, path: str, db_name: str) -> None:
         """
         Instance few variables and create database if not exists
         @param => str: `path`: Path to the folder who the database is or are going to was write
@@ -34,16 +35,14 @@ class Database:
                 if f.read(2) == b"\n":
                     self.CreateDatabase()
 
-
-    def ConnectDatabase(self):
+    def ConnectDatabase(self) -> None:
         """
         Connect to the database and set a cursor
         """
         self.conn = sqlite3.connect(self.full_path)
         self.cursor = self.conn.cursor()
 
-
-    def CloseDatabase(self):
+    def CloseDatabase(self) -> None:
         """
         Commit and close connection to the database
         """
@@ -56,23 +55,23 @@ class Database:
         # Unset variable
         self.conn, self.cursor = None, None
 
-
     @database_invoke
-    def CreateDatabase(self):
+    def CreateDatabase(self) -> None:
         """
         Create the database using the full_path variable
         """
         # Create table
         self.cursor.execute(SQLITE_CREATE_DATABASE)
 
-
     @database_invoke
-    def isNewsExists(self, root, name, title, hash_description, link):
+    def isNewsExists(self, root: str, name: str, title: str, hash_description: str, link: str) -> bool:
         """
-        Return true if the news exists
+        @param => str: `root`: Root `name` of the news.
+        @param => str: `name`: Name of the category for the news.
         @param => str: `title`: Title of the news.
         @param => str: `hash_description`: sha256 hexdigest of the description.
         @param => str: `link`: Link of the news.
+        :returns True if the news exists
         """
         # Arguments
         args = tuple(locals().values())[1:]
@@ -87,9 +86,8 @@ class Database:
             return False
         return True
 
-
     @database_invoke
-    def AddNews(self, root, name, title, hash_description, link):
+    def AddNews(self, root: str, name: str, title: str, hash_description: str, link: str) -> None:
         """
         @param => str: `root`: root name set in const.
         @param => str: `name`: Name set in const.
@@ -102,10 +100,9 @@ class Database:
 
         # Execute the query
         self.cursor.execute(SQLITE_INSERT_NEWS, args)
-    
 
     @database_invoke
-    def DeleteEntries(self, root, name):
+    def DeleteEntries(self, root: str, name: str) -> None:
         """
         @param => str: `root`: root name set by the user in const.
         @param => str: `name`: name of the subsection set by the user in const.
